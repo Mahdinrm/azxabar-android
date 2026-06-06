990109019728b730af170b9a699c008e21171e97
Mahdi NARIMANI <azxabarnews@gmail.com>
2026-06-06 20:34:01 +0200

    Refactor back button handling and enable fullscreen mode
    Removed custom back button handling and added fullscreen system UI settings.

--- app/src/main/java/com/azxabar/app/MainActivity.java
+++ app/src/main/java/com/azxabar/app/MainActivity.java
@@ -15,7 +15,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
-import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -24,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private SwipeRefreshLayout swipeRefresh;
     private ProgressBar progressBar;
-
     private static final String APP_HTML = "file:///android_asset/index.html";
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -32,9 +30,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Tam ekran - sistem çubuklarını gizle
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(Color.parseColor("#07090F"));
+        window.setNavigationBarColor(Color.parseColor("#07090F"));
+        window.getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
 
         FrameLayout root = new FrameLayout(this);
         root.setBackgroundColor(Color.parseColor("#07090F"));
@@ -114,16 +119,13 @@ public class MainActivity extends AppCompatActivity {
 
         swipeRefresh.setOnRefreshListener(() -> webView.reload());
 
-        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                if (webView.canGoBack()) {
-                    webView.goBack();
-                }
-            }
-        });
-
         webView.loadUrl(APP_HTML);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Geri tuşu - direkt uygulamadan çık
+        super.onBackPressed();
     }
 
     @Override
